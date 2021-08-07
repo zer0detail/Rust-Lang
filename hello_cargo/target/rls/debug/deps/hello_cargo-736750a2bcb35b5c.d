73804bca3cac45f845a023effbc09a4d696bc589
Zer0detail <zer0detail@protonmail.com>
2021-08-07 11:30:24 +1000

    initial commit

---
+++ hello_cargo/target/rls/debug/deps/hello_cargo-736750a2bcb35b5c.d
@@ -0,0 +1,5 @@
+c:\Users\Aaron\Desktop\RustStuff\projects\hello_cargo\target\rls\debug\deps\hello_cargo-736750a2bcb35b5c.rmeta: src\main.rs
+
+c:\Users\Aaron\Desktop\RustStuff\projects\hello_cargo\target\rls\debug\deps\hello_cargo-736750a2bcb35b5c.d: src\main.rs
+
+src\main.rs:
